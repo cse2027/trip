7c344a726782f35453e8f859f6ec32adbcdfc176
cse2027 <cseab2027@gmail.com>
2026-01-03 20:18:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  document.body.style.overflow = "auto";
+}, 2000);
